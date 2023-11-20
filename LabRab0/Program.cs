260d0348a0cc6e8827e454d4636c3622f837eba7
Анастасия <@COMPUTER>
2023-11-20 17:05:38 +0700

    Добавьте файлы проекта.

---
+++ LabRab0/Program.cs
@@ -0,0 +1,19 @@
+// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace LabRab0
+{
+    /// <summary>
+    /// Вводное задание.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Основная функция.
+        /// </summary>
+        private static void Main()
+        {
+        }
+    }
+}
